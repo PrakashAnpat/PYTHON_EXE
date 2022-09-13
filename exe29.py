065bbf1eb30cad9fceeedcd5f4e3838a4c4a5f2b
PrakashAnpat <prakashp17696@gmail.com>
2022-09-13 13:35:30 +0530

    if conditional statement

---
+++ exe29.py
@@ -0,0 +1,25 @@
+people = 20
+cats = 30
+dogs = 15
+if people < cats: # Use if conditional statement & also use comparision operators.
+    print("Too many cats! The world is doomed!")
+if people > cats:
+    print("Not many cats!The world is saved")
+if people < dogs:
+    print("The world is drooled on!")
+if people > dogs:
+    print("The world is dry!")
+if (people == cats or people < cats): # Logical comparision operators.
+    print("True")   
+if not (people == dogs):
+    print("True")  
+dogs += 5 # Increment operator.
+if people >= dogs:
+    print("People are grater than or equal to dogs.")
+if people <= dogs:
+    print("People are less than or equal to dogs.")
+if people == dogs:
+    print("People are dogs.")
+#if (people < cats and people == cats):
+#    print("False") 
+
